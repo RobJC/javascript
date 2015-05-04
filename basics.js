472fd376c6e5bb3ec891e676ab7b01e0f20a8664
Jose Sanchez <jparkers0012@gmail.com>
2015-05-04 14:01:47 +0000

    Closures

--- basics.js
+++ basics.js
@@ -37,3 +37,27 @@ console.log(name);
 for(var number = 0 ; number <= 5 ; number += 1){
     console.log(number);
 }
+
+// *********************************** CLOSURE
+function wrapValue(n) {
+  var localVariable = n;
+  return function() { return localVariable; };
+}
+var wrap1 = wrapValue(1);
+var wrap2 = wrapValue(2);
+console.log(wrap1());
+// → 1
+console.log(wrap2());
+// → 2
+
+// MORE
+function multiplier(factor) {
+  return function(number) {
+    return number * factor;
+  };
+}
+
+var twice = multiplier(2);
+console.log(twice(5));
+// → 10
+
